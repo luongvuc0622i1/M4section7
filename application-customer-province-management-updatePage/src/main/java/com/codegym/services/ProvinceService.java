5d215417d91c2de003f4c621a1e2d7c8244d61df
luongvuc0622i1 <vuhienluong.td151218@gmail.com>
2022-10-24 07:06:01 +0700

    thuc hanh 2

---
+++ application-customer-province-management-updatePage/src/main/java/com/codegym/services/ProvinceService.java
@@ -0,0 +1,13 @@
+package com.codegym.services;
+
+import com.codegym.model.Province;
+
+public interface ProvinceService {
+    Iterable<Province> findAll();
+
+    Province findById(Long id);
+
+    void save(Province province);
+
+    void remove(Long id);
+}
